ed4a3865ddc468851a15aa74f00b1333f748f800
kinokoTakenoko35 <kinoko35@me.com>
2021-07-20 21:46:29 +0900

    fetch products from local server.

--- framework/shopify/product/get-all-product.ts
+++ framework/shopify/product/get-all-product.ts
@@ -1,19 +1,33 @@
-const fetchApi = async () => {
-  const url = "https://jsonplaceholder.typicode.com/todos";
+import getAllProductsQuery from "../utils/queries/get-all-product";
+
+// 仮引数のType定義
+type FetchParams = {
+  query: string;
+};
+
+// 非同期宣言したGraphQLをフェッチする関数。仮引数にQueryをいれる
+const fetchApi = async ({ query }: FetchParams) => {
+  const url = "https://localhost:400/graphql";
 
   const res = await fetch(url, {
-    method: "GET",
+    // リクエストメソッドPOSTに変更
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    // bodyはJSON形式で取得
+    body: JSON.stringify({
+      query,
+    }),
   });
 
   const data = await res.json();
   return { data };
 };
 
+// fetchApiで取得したデータをProductのデータとして返す関数
 const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi();
+  const products = await fetchApi({ query: getAllProductsQuery });
   return products.data;
 };
 
