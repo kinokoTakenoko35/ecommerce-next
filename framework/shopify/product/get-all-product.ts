6021e11616efa5e9241c1e3f1e4983f2bb267cc3
kinokoTakenoko35 <kinoko35@me.com>
2021-07-21 09:35:58 +0900

    specify fetcher return type

--- framework/shopify/product/get-all-product.ts
+++ framework/shopify/product/get-all-product.ts
@@ -1,10 +1,19 @@
 import fetchApi from "../utils/fetch-api";
 import getAllProductsQuery from "../utils/queries/get-all-product";
+import { ProductConnection } from "../schema";
+
+// products情報を取得するタイプを宣言
+type ReturnType = {
+  products: ProductConnection;
+};
 
 // fetchApiで取得したデータをProductのデータとして返す関数
-const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi({ query: getAllProductsQuery });
-  return products.data;
+const getAllProducts = async (): Promise<any> => {
+  // data = ReturnType
+  const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+
+  // normalize and return new data!
+  return data.products;
 };
 
 export default getAllProducts;
